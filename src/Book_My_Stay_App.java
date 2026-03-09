05a092336211429e1a720b839152b3c024820628
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-03-09 18:39:46 +0530

    Use Case 3: Centralized Room Inventory Management

--- src/Book_My_Stay_App.java
+++ src/Book_My_Stay_App.java
@@ -1,14 +1,39 @@
+import java.util.*;
+
 /**
- * MAIN CLASS Book_My_Stay_App
- *
- * Use Case 2: Basic Room Types & Static Availability
- *
- * Description:
- * This abstract class represents a generic hotel room.
- *
+ * Class RoomInventory
+ Use Case 3: Centralized Room Inventory Management
+
+ *Description:
+ *This class demonstrates how room availability is managed using a centralized inventory.
+ *Room objects are used to retrieve pricing and room characteristics.
+ *No booking or search logic is introduced here.
  * @author Kanishk Upadhyay
  * @version 2.0
  */
+class RoomInventory{
+    Map<String,Integer> inventory=new HashMap<>();
+
+    public RoomInventory(){
+        inventory.put("SingleRoom",3);
+        inventory.put("DoubleRoom",3);
+        inventory.put("SuiteRoom",2);
+    }
+
+    public void DisplayInventory(){
+        for(Map.Entry<String,Integer> entry: inventory.entrySet()){
+            System.out.println(entry.getKey()+" : "+entry.getValue());
+        }
+    }
+
+    public int GetAvailablity(String roomType){ //get the availablity based on room type
+        return inventory.get(roomType);
+    }
+
+    public void UpdateAvailability(String roomType,int count){  //updating the avaiblity
+        inventory.put(roomType,count);
+    }
+}
 
 abstract class Room{
     protected int NoOfBeds;
@@ -33,7 +58,7 @@ abstract class Room{
 
 // Double Room
 class DoubleRoom extends Room{
-    int Available = 3;
+
 
     public DoubleRoom(){
         super(2,400,2500);
@@ -43,15 +68,14 @@ class DoubleRoom extends Room{
     public void DisplayRoomDetails(){
         System.out.println("Double Room:\nNo of beds: "+NoOfBeds+
                 "\nRoom size: "+SquareFeet+" SqFeet"+
-                "\nRent per Night: ₹"+PricePerNight+
-                "\nAvailable : "+Available);
+                "\nRent per Night: ₹"+PricePerNight);
     }
 }
 
 
 // Single Room
 class SingleRoom extends Room{
-    int Available = 3;
+
 
     public SingleRoom(){
         super(1,250,1500);
@@ -61,15 +85,14 @@ class SingleRoom extends Room{
     public void DisplayRoomDetails(){
         System.out.println("Single Room:\nNo of beds: "+NoOfBeds+
                 "\nRoom size: "+SquareFeet+" SqFeet"+
-                "\nRent per Night: ₹"+PricePerNight+
-                "\nAvailable : "+Available);
+                "\nRent per Night: ₹"+PricePerNight);
     }
 }
 
 
 // Suite Room
 class SuiteRoom extends Room{
-    int Available = 2;
+
 
     public SuiteRoom(){
         super(3,750,5000);
@@ -79,8 +102,7 @@ class SuiteRoom extends Room{
     public void DisplayRoomDetails(){
         System.out.println("Suite Room:\nNo of beds: "+NoOfBeds+
                 "\nRoom size: "+SquareFeet+" SqFeet"+
-                "\nRent per Night: ₹"+PricePerNight+
-                "\nAvailable : "+Available);
+                "\nRent per Night: ₹"+PricePerNight);
     }
 }
 
@@ -91,17 +113,30 @@ public class Book_My_Stay_App {
 
         System.out.println("Welcome To Hotel Booking Management System");
         System.out.println("System Initialized Successfully.");
-        System.out.println("Version: 2.0\n");
+        System.out.println("Version: 3.0\n");
+
+        RoomInventory inventory=new RoomInventory();
+        System.out.println("Current room Inventory");
+        inventory.DisplayInventory();
 
         SingleRoom r1 = new SingleRoom();
         r1.DisplayRoomDetails();
+        System.out.println("Availablity: "+inventory.GetAvailablity("SingleRoom"));
         System.out.println();
 
         DoubleRoom r2 = new DoubleRoom();
         r2.DisplayRoomDetails();
+        System.out.println("Availablity: "+inventory.GetAvailablity("DoubleRoom"));
         System.out.println();
 
         SuiteRoom r3 = new SuiteRoom();
         r3.DisplayRoomDetails();
+        System.out.println("Availablity: "+inventory.GetAvailablity("SuiteRoom"));
+        System.out.println();
+
+        inventory.UpdateAvailability("SingleRoom",2);
+
+        System.out.println("\nInventory After Booking:");
+        inventory.DisplayInventory();
     }
 }
